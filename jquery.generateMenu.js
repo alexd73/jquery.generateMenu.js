e44a400a0b8bd973bd8b913fad174fc8079b1458
alexd <alexd73@gmail.com>
2016-05-02 23:16:16 +0300

    Version 1

--- jquery.generateMenu.js
+++ jquery.generateMenu.js
@@ -1,3 +1,36 @@
 /**
  * Created by alexey on 02.05.16.
  */
+
+(function( $ ){
+	/**
+	 * Плагин необходимо навешивать на пустой элемент <ul>, в нем будут сгенерированы ссылки.
+	 * Пример вызова:
+	 * 	$("#navbar ui.nav).generateMenu();
+	 * @returns {boolean}
+     */
+    $.fn.generateMenu = function() {
+
+		// Создаём настройки по-умолчанию, расширяя их с помощью параметров, которые были переданы
+		var options = $.extend( {
+			"defSection": "section",
+			"liClasses": ""
+		}, options);
+
+		t = this;
+		$(options.defSection).each( function(index, element) {
+			console.log( "id:", $(element).attr("id"));
+			var linkText = $(element).data("linkText");
+			var href = element.id;
+			item = "";
+			if (typeof linkText != 'undefined') {
+				item = "<li><a href=\"#" + href + "\">" + linkText + "</a></li>";
+				t.append(item);
+				console.log(linkText)
+			} else {
+				console.log("no link text")
+			}
+		});
+        return true;
+    };
+})( jQuery );
